bcffa0c5fbc7c62587b252a5dd7481b16c261d55
Sakariyah Abdulhazeem <sakariyahabduhazeem@gmail.com>
2025-12-07 09:55:05 +0100

    Refactor AuthNavButtons to use Next.js Link component for navigation. Updated "Book a Demo" and "Login" buttons to link to their respective pages.

--- components/AuthNavButtons.tsx
+++ components/AuthNavButtons.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import Button from "./ui/button";
 
 export default function AuthNavButtons() {
@@ -12,11 +13,11 @@ export default function AuthNavButtons() {
             <div className="flex items-center gap-5 ">
                 <Button
                     className="bg-blue-600 hover:bg-blue-400 text-white px-10 py-3 animate-in-out transition-all ">
-                    Book a Demo
+                   <Link href="/request-a-demo">Book a Demo</Link>
                 </Button>
                 <Button
                     className="bg-white hover:bg-blue-600 border-2 border-blue-500  text-blue-500 hover:text-white px-10 py-3 animate-in-out transition-all">
-                    Login
+                  <Link href="/login">Login</Link> 
                 </Button>
             </div>
 
